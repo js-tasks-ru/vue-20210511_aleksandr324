43d4d91f4f10a12526b58a204f6a0e9a6888aa0c
Aleksandr <nithogg@mail.ru>
2021-05-20 00:21:02 +0300

    Основы 3

--- 01-basics/03-MeetupTitle/script.js
+++ 01-basics/03-MeetupTitle/script.js
@@ -1,3 +1,18 @@
-// import Vue from './vendor/vue.esm.browser.js';
+import Vue from './vendor/vue.esm.browser.js';
 
-// Требуется создать Vue приложение
+const buttons = [1, 2, 3, 4, 5];
+
+
+const app = new Vue({
+  el: '#app',
+  data: {
+    selecting: null,
+    items: []
+  },
+  mounted: async function() {
+    for (const v of buttons) {
+      const result = await fetch(`https://course-vue.javascript.ru/api/meetups/${v}`).then((res) => res.json());
+      this.items.push({ buttonNumber: v, courseName: result.title });
+    }
+  }
+});
