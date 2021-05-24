ef6e0122dbb1bae71cd428883a48aa1b6d8faae7
Aleksandr <nithogg@mail.ru>
2021-05-24 16:21:53 +0300

    Исправления основы 2-3, добавлена 4

--- 01-basics/03-MeetupTitle/script.js
+++ 01-basics/03-MeetupTitle/script.js
@@ -1,18 +1,21 @@
 import Vue from './vendor/vue.esm.browser.js';
 
-const buttons = [1, 2, 3, 4, 5];
-
-
 const app = new Vue({
   el: '#app',
   data: {
     selecting: null,
-    items: []
+    courseName: null
+     },
+
+  methods: {
+    async selectResult() {
+      const result = await fetch(`https://course-vue.javascript.ru/api/meetups/${this.selecting}`).then((res) => res.json());
+      this.courseName = result.title;
+    }
   },
-  mounted: async function() {
-    for (const v of buttons) {
-      const result = await fetch(`https://course-vue.javascript.ru/api/meetups/${v}`).then((res) => res.json());
-      this.items.push({ buttonNumber: v, courseName: result.title });
+  watch: {
+    selecting() {
+      this.selectResult();
     }
   }
 });
--- 01-basics/04-MeetupPageDraft/script.js
+++ 01-basics/04-MeetupPageDraft/script.js
@@ -1,7 +1,7 @@
-import Vue from './vendor/vue.esm.browser.js';
+import Vue from "./vendor/vue.esm.browser.js";
 
 /** URL адрес API */
-const API_URL = 'https://course-vue.javascript.ru/api';
+const API_URL = "https://course-vue.javascript.ru/api";
 
 /** ID митапа для примера; используйте его при получении митапа */
 const MEETUP_ID = 6;
@@ -15,18 +15,21 @@ function getImageUrlByImageId(imageId) {
   return `${API_URL}/images/${imageId}`;
 }
 
+const fetchMeetups = () =>
+  fetch(`${API_URL}/meetups/${MEETUP_ID}`).then((res) => res.json());
+
 /**
  * Словарь заголовков по умолчанию для всех типов пунктов программы
  */
 const agendaItemDefaultTitles = {
-  registration: 'Регистрация',
-  opening: 'Открытие',
-  break: 'Перерыв',
-  coffee: 'Coffee Break',
-  closing: 'Закрытие',
-  afterparty: 'Afterparty',
-  talk: 'Доклад',
-  other: 'Другое',
+  registration: "Регистрация",
+  opening: "Открытие",
+  break: "Перерыв",
+  coffee: "Coffee Break",
+  closing: "Закрытие",
+  afterparty: "Afterparty",
+  talk: "Доклад",
+  other: "Другое"
 };
 
 /**
@@ -34,14 +37,43 @@ const agendaItemDefaultTitles = {
  * Соответствует имени иконок в директории /assets/icons
  */
 const agendaItemIcons = {
-  registration: 'key',
-  opening: 'cal-sm',
-  talk: 'tv',
-  break: 'clock',
-  coffee: 'coffee',
-  closing: 'key',
-  afterparty: 'cal-sm',
-  other: 'cal-sm',
+  registration: "key",
+  opening: "cal-sm",
+  talk: "tv",
+  break: "clock",
+  coffee: "coffee",
+  closing: "key",
+  afterparty: "cal-sm",
+  other: "cal-sm"
 };
 
-// Требуется создать Vue приложение
+
+const app = new Vue({
+  el: "#app",
+  data: {
+    meetapInfo: null,
+    agendaItemIcons: agendaItemIcons,
+    agendaItemDefaultTitles: agendaItemDefaultTitles
+  },
+  mounted() {
+    fetchMeetups().then((fetchMeetups) => {
+    this.meetapInfo = {
+      title: fetchMeetups.title,
+      place: fetchMeetups.place,
+      organizer: fetchMeetups.organizer,
+      description: fetchMeetups.description,
+      date: new Date(fetchMeetups.date),
+      cover: fetchMeetups.imageId && `https://course-vue.javascript.ru/api/images/${fetchMeetups.imageId}`,
+      coverStyle: fetchMeetups.imageId && { "--bg-url": `url(${getImageUrlByImageId(`${fetchMeetups.imageId}`)})` },
+      localeDate: new Date(fetchMeetups.date).toLocaleString(navigator.language, {
+        year: "numeric",
+        month: "long",
+        day: "numeric"
+      }),
+      dateOnlyString: new Date(fetchMeetups.date).toISOString().split("T")[0],
+      agenda: fetchMeetups.agenda
+    };
+  })
+  }
+
+});
